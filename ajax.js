16567fbd2571972bfda536819aac65f4998fb8cf
agasca <profesorweb@yahoo.com>
2014-12-10 18:47:54 -0600

    geolocalizacion ajax

--- ajax.js
+++ ajax.js
@@ -1,3 +1,56 @@
 $(function(){
-	
+	//peticion get/post
+	//     recurso a obtener   callback cuando ajax termina
+	$.get('logos_footer.html', function(codigo_logo){
+		//console.log(codigo_logo);
+		$('footer').append(codigo_logo);
+	});
+
+
+
+	//objeto
+	$.get('usuario.json', function(codigo_dato){
+		//console.log(codigo_dato);
+		var avatar = new Image();
+		avatar.src = codigo_dato.avatar;
+		avatar.title = codigo_dato.nombre+' '+codigo_dato.apellido;
+				
+		$('#avatar').append(avatar);
+	});
+
+
+	//obtenerGeoInformacion();
+
+	//function obtenerGeoInformacion(lat, lon){
+	//function obtenerGeoInformacion(){
+	//	console.log(window.lat, window.lon);
+	//}
+
+	//carga html
+	/*
+	$('footer').load('logos_footer.html #logo_1')
+	*/
 });
+
+var base_url = "https://query.yahooapis.com/v1/public/yql?";
+
+function obtenerGeoInformacion(lat, lon){
+	//console.log(lat, lon);
+	var query = 'select * from geo.placefinder where text = "'+lat+','+lon+'" and gflags = "R"';
+	//console.log(query);
+	query = encodeURIComponent(query);
+	//console.log(query);
+
+	$.ajax({
+		url: base_url+"q="+query,
+		dataType : 'jsonp',
+		jsonCallBack : 'procesarGeoInfo',
+		data : {
+			format : 'json'
+		}
+	});
+}
+
+function procesarGeoInfo(datos){
+	console.log(datos);
+}
--- geo.js
+++ geo.js
@@ -15,6 +15,10 @@ $(function(){
 		var mapa = new Image();
 		mapa.src = "http://maps.googleapis.com/maps/api/staticmap?zoom=15&maptype=hybrid&size=200x200&sensor=false&center="+lat+","+lon;
 		$('#geo').append(mapa);
+
+		obtenerGeoInformacion(lat, lon);
+		//obtenerGeoInformacion(lan, lon)	//funcion oculta llama ajax.js
+		//window.lat = lat; window.lon = lon;
 	}
 
 	geo.getCurrentPosition(geo_exito, geo_error, opciones);
